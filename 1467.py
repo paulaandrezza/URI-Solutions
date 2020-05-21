d18997ba77fbb6a1fc7a736aa113ef2d1b1db289
Paula Marinho <paulaandrezza25@gmail.com>
2020-05-21 18:00:48 -0300

    Add existing file

---
+++ 1467.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+
+        a, b, c = [int(i) for i in input().split()]
+
+        if a == b == c:
+            print("*")
+        elif b == c:
+            print("A")
+        elif a == c:
+            print("B")
+        else:
+            print("C")
+
+    except EOFError:
+        break
